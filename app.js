78b843ffad6a8c653c0ba4aa94606cd7d9c34619
Juan Pablo Raccuia <juanpabloraccuia9@gmail.com>
2022-09-05 17:40:05 -0300

    install npm

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const countries = require('./countries.json');
  * @returns {Object}
  */
 function getCountryByIso3(isoCode){
-    //Complete with your code
+    console.log(isoCode);
+    return isoCode;
+    return 
 }
 
 /**
